9fa75d237767ed2a363730266c8aac7e7c0ab64a
Marcin <marcin@opensoftware.pl>
2020-01-14 15:21:23 +0100

    Add BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import axios from 'axios'
 import VueSession from 'vue-session'
 
@@ -9,6 +14,7 @@ import router from './router'
 import store from './store'
 
 Vue.use(ElementUI)
+Vue.use(BootstrapVue)
 Vue.use(VueSession)
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
