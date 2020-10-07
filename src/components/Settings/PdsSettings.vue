01a650dbeee22accaa995abf4f11b5e1a9f358aa
Marcin <marcin@opensoftware.pl>
2020-10-07 17:55:27 +0200

    Add PdsPlugins mock in Settings module

--- src/components/Settings/PdsSettings.vue
+++ src/components/Settings/PdsSettings.vue
@@ -6,8 +6,13 @@
 
     <div class="content">
       <current-pds />
-      <pds-plugins />
+      <pds-plugins
+        @configure-plugin="previewPluginConfig($event)" />
     </div>
+
+    <preview-component ref="PluginConfigPreviewComponent"
+      :readonly="false" confirmLabel="Connect"
+      :form="pluginForm" :alternatives="pluginAlternatives"></preview-component>
   </div>
 </template>
 
@@ -15,18 +20,37 @@
 import CurrentPds from './PdsSettings/CurrentPds'
 import PdsPlugins from './PdsSettings/PdsPlugins'
 
+import { eventBus as ocaEventBus, EventHandlerConstant,
+  PreviewComponent } from 'odca-form'
+
 export default {
   name: 'pds-settings',
   props: ['title'],
   components: {
     CurrentPds,
-    PdsPlugins
+    PdsPlugins,
+    PreviewComponent
   },
   data() {
     return {
+      pluginForm: {},
+      pluginAlternatives: []
     }
   },
   methods: {
+    connectPlugin(data) {
+      console.log(data)
+      this.$noty.success("Plugin Connected!", { timeout: 1000 })
+      this.$refs.PluginConfigPreviewComponent.closeModal()
+    },
+    previewPluginConfig(event) {
+      this.pluginAlternatives = event.plugin.formAlternatives
+      this.$refs.PluginConfigPreviewComponent.openModal(event.plugin.form)
+    },
+  },
+  mounted() {
+    ocaEventBus.$off(EventHandlerConstant.SAVE_PREVIEW)
+    ocaEventBus.$on(EventHandlerConstant.SAVE_PREVIEW, this.connectPlugin)
   }
 }
 </script>
--- src/components/Settings/PdsSettings/PdsPlugins.vue
+++ src/components/Settings/PdsSettings/PdsPlugins.vue
@@ -1,19 +1,101 @@
 <template>
   <div>
-    Plugins
+    Plugins:<br>
+    <el-collapse v-model="expanded_items">
+      <ul class="list">
+        <el-collapse-item
+          v-for="(plugin, index) in plugin_list"
+          :title="plugin.pluginName"
+          :name="plugin.pluginName + index"
+          :key="index">
+          <el-row>
+            <vue-json-pretty :deep=0 :data="plugin" />
+            <el-button size="medium"
+              @click="configure(plugin)">Configure</el-button>
+          </el-row>
+        </el-collapse-item>
+      </ul>
+    </el-collapse>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { renderForm } from 'odca-form'
+import VueJsonPretty from 'vue-json-pretty';
+
 export default {
   name: 'pds-plugins',
   components: {
+    VueJsonPretty
   },
   data() {
     return {
+      expanded_items: [],
+      plugin_list: [
+        { pluginName: 'plug1', oca_schema_namespace: 'consent', oca_schema_dri: 'fArVHJTQSKHu2CeXJocQmH3HHxzZXsuQD7kzyHJhQ49s' },
+        { pluginName: 'plug2', oca_schema_namespace: 'consent', oca_schema_dri: 'fArVHJTQSKHu2CeXJocQmH3HHxzZXsuQD7kzyHJhQ49s' },
+      ]
+    }
+  },
+  computed: {
+    ocaRepoUrl: function() {
+      return `${config.env.VUE_APP_PROTOCOL}://${config.env.VUE_APP_OCA_REPO}.${config.env.VUE_APP_HOST}`
     }
   },
   methods: {
+    async renderPluginForm(plugin) {
+      const pluginBranch = (await axios.get(
+        `${this.ocaRepoUrl}/api/v2/schemas/${plugin.oca_schema_namespace}/${plugin.oca_schema_dri}`
+      )).data
+
+      const pluginLangBranches = this.splitBranchPerLang(pluginBranch)
+      let pluginForm
+      const pluginFormAlternatives = []
+      try {
+        pluginLangBranches.forEach(langBranch => {
+          pluginFormAlternatives.push({
+            language: langBranch.lang,
+            form: renderForm([langBranch.branch.schema_base, ...langBranch.branch.overlays]).form
+          })
+        })
+        pluginForm = pluginFormAlternatives[0].form
+      } catch(e) {
+        console.log(e)
+        this.$noty.error("ERROR! Consent form data are corrupted.", {
+          timeout: 1000
+        })
+      }
+
+      return {
+        form: pluginForm,
+        formAlternatives: pluginFormAlternatives
+      }
+    },
+    splitBranchPerLang(branch) {
+      const langBranches = []
+      const labelOverlays = branch.overlays.filter(o => o.type.includes("label"))
+      const languages = labelOverlays.map(o => o.language)
+      const schemaBase = branch.schema_base
+      languages.forEach(lang => {
+        langBranches.push({
+          lang: lang,
+          branch: {
+            schema_base: schemaBase,
+            overlays: branch.overlays.filter(o => {
+              if(!o.language) { return true }
+              return o.language == lang
+            })
+          }
+        })
+      })
+      return langBranches
+    },
+    async configure(plugin) {
+      this.$emit('configure-plugin', {
+        plugin: await this.renderPluginForm(plugin)
+      })
+    }
   }
 }
 </script>
