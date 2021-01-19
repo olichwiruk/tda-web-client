d30b51c3f6200653257a824ff2549ef10c498583
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2021-01-19 13:20:24 +0100

    Ported request for presentation to new UI

--- src/components/Connections/ConnectionList/PresentationRequest/Dialog.vue
+++ src/components/Connections/ConnectionList/PresentationRequest/Dialog.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <q-dialog
-      v-model="active"
+      v-model="isDialogVisible"
       persistent
-      @close="deactivate()"
     >
       <q-card>
         <q-card-section>
@@ -11,28 +10,11 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <!-- <vue-typeahead-bootstrap -->
-          <!--   v-model="search.query" -->
-          <!--   :minMatchingChars="0" -->
-          <!--   :showOnFocus="true" -->
-          <!--   :data="search.matching.length == 0 ? search.all : search.matching" -->
-          <!--   :serializer="s => `${s.namespace} / ${s.schemaName}`" -->
-          <!--   @hit="getOcaSchema" -->
-          <!--   style="width: 100%;"> -->
-
-          <!--   <template slot="append"> -->
-          <!--     <el-button -->
-          <!--     slot="append" -->
-          <!--     class="btn-sm" -->
-          <!--     icon="el-icon-search" -->
-          <!--     v-show="search.query && ocaForm" -->
-          <!--     @click="preview()">Preview</el-button> -->
-          <!--   </template> -->
-
-          <!--   <template slot="suggestion" slot-scope="{ data, htmlText }"> -->
-          <!--     <span v-html="htmlText"></span> -->
-          <!--   </template> -->
-          <!-- </vue-typeahead-bootstrap> -->
+          <oca-schema-search
+            label="Schema"
+            :ocaRepoHost="ocaRepoHost"
+            @schemaSelected="(form) => ocaForm = form"
+          />
         </q-card-section>
 
         <q-card-actions align="right">
@@ -53,133 +35,48 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
-
-    <preview-component
-      style="z-index: 9999;"
-      ref="PreviewComponent"
-      :readonly="true"
-      :form="ocaForm"
-      :alternatives="ocaFormAlternatives"
-    ></preview-component>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-//import { renderForm, PreviewComponent } from 'odca-form'
-
-//import VueTypeaheadBootstrap from 'vue-typeahead-bootstrap'
+import OcaSchemaSearch from '@/components/Services/NewService/OcaSchemaSearch.vue'
 
 export default {
   name: 'presentation-request-dialog',
   props: { title: String },
-  //components: { VueTypeaheadBootstrap, PreviewComponent },
+  components: { OcaSchemaSearch },
   data() {
     return {
-      active: false,
-      search: {
-        query: '',
-        all: [],
-        matching: [],
-        selected: null
-      },
+      isDialogVisible: false,
       ocaForm: null,
-      ocaFormAlternatives: [],
       formLabelWidth: '200px',
     }
   },
+  watch: {
+    isDialogVisible() {
+      // always reset the dialog when it is (re-)opened
+      if (this.isDialogVisible)
+        this.ocaForm = null;
+    }
+  },
   computed: {
-    ocaRepoUrl: function() {
+    ocaRepoUrl: function () {
       return this.$session.get('ocaRepoUrl')
     },
-  },
-  watch: {
-    'search.query': function(input) {
-      if(input.length == 0) {
-        this.ocaForm = null
-      }
-      this.fetchOcaSchemas(input)
+    ocaRepoHost: function () {
+      return this.$session.get('ocaRepoUrl')
     },
   },
   methods: {
     deactivate() {
-      this.active = false
+      this.isDialogVisible = false
     },
     request_pesentation() {
       this.$emit('presentation-requested', {
-        oca_schema_dri: this.search.selected.DRI
+        oca_schema_dri: this.ocaForm.DRI,
       })
       this.deactivate()
-    },
-    fetchOcaSchemas: function(input) {
-      axios.get(`${this.ocaRepoUrl}/api/v2/schemas?suggest=${input}`)
-        .then(r => {
-          this.search.matching = r.data.map(x => {
-            return {
-              namespace: x.namespace,
-              DRI: x.DRI,
-              schemaName: x.schema.name
-            }
-          })
-        })
-    },
-    getOcaSchema: async function(schema) {
-      this.ocaFormAlternatives = []
-      const result = await axios.get(`${this.ocaRepoUrl}/api/v2/schemas?_index=branch&schema_base=${schema.DRI}`)
-      const branchesBase = result.data.filter(e => e.namespace == schema.namespace)
-      const branchBase = branchesBase[0]
-      this.search.selected = {
-        namespace: branchBase.namespace,
-        DRI: branchBase.DRI,
-        schemaName: schema.schemaName
-      }
-
-      const branchResponse = await axios.get(`${this.ocaRepoUrl}/api/v2/schemas/${branchBase.namespace}/${branchBase.DRI}`)
-      const branch = branchResponse.data
-      const langBranches = this.splitBranchPerLang(branch)
-
-      try {
-        langBranches.forEach(langBranch => {
-          this.ocaFormAlternatives.push({
-            language: langBranch.lang,
-            form: renderForm([langBranch.branch.schema_base, ...langBranch.branch.overlays]).form
-          })
-        })
-
-        this.ocaForm = this.ocaFormAlternatives[0].form
-      } catch(e) {
-        this.$noty.error("ERROR! Form data are corrupted.", {
-          timeout: 1000
-        })
-      }
-    },
-    splitBranchPerLang: function(branch) {
-      const langBranches = []
-      const labelOverlays = branch.overlays.filter(o => o.type.includes("label"))
-      const languages = labelOverlays.map(o => o.language)
-      const schemaBase = branch.schema_base
-      languages.forEach(lang => {
-        langBranches.push({
-          lang: lang,
-          branch: {
-            schema_base: schemaBase,
-            overlays: branch.overlays.filter(o => {
-              if(!o.language) { return true }
-              return o.language == lang
-            })
-          }
-        })
-      })
-      return langBranches
-    },
-    preview(formInput = null) {
-      try {
-        this.$refs.PreviewComponent.openModal(this.ocaForm, formInput);
-      } catch(e) {
-        this.$noty.error("ERROR! Form data are corrupted.", {
-          timeout: 1000
-        })
-      }
     },
   }
 }
