67ff617c0f2ee9d3fc0728b8751b4fbc09efb315
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-02-17 14:31:54 +0100

    Add login screen

---
+++ src/pages/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="center">
+        <q-card flat>
+          <header class="text-h4 text-center q-pa-lg">
+            Trusted Digital Assistant
+          </header>
+          <q-linear-progress indeterminate />
+          <div class="q-pa-lg space-between">
+            <q-img
+              class="q-ma-md logo-image"
+              :src="oydImageUrl"
+            />
+            <q-img
+              class="q-ma-md logo-image"
+              :src="hcfImageUrl"
+            />
+          </div>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import hcfLogo from '../assets/logo_hcf.png'
+import oydLogo from '../assets/logo_oyd.png'
+
+export default class Login extends Vue {
+  get hcfImageUrl () { return hcfLogo }
+  get oydImageUrl () { return oydLogo }
+}
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.space-between {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,13 @@ import { RouteRecordRaw } from 'vue-router'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    name: 'login',
+    component: () => import('pages/Login.vue')
+  },
+  {
+    path: '/agent/:id',
+    name: 'agent',
+    props: true,
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
